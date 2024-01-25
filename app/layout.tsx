a6d243f997940f7ad61d96cba1fff669f8401a65
Ose Ogbe <therealnylat@gmail.com>
2024-01-25 13:02:47 +0100

    create store

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ToasterProvider } from "@/providers/toast-provider";
+import { ModalProvider } from "@/providers/modal-provider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "E-commerce Admin Dashboard",
-  description: "An e-commerce admin dashboard built with Next 14, React, Tailwind, Clerk",
+  description: "An e-commerce admin dashboard built with Next 14, React, TailwindCSS, Clerk, Zustand, React Hook Form, Zod, Prisma, PlanetScale",
 };
 
 export default function RootLayout({
@@ -18,7 +22,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
